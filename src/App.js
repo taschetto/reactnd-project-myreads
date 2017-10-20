a9454bcf4d253d156f2b0e940d694539864df008
Guilherme Taschetto <gtaschetto@gmail.com>
2017-10-20 00:35:34 -0200

    feat: Add dynamic document title to each component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import DocumentTitle from 'react-document-title'
 import SearchBooks from './components/SearchBooks'
 import ListBooks from './components/ListBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    query: 'rowling'
+  }
+
+  updateQuery = query => {
+    this.setState({ query })
+  }
+
   render() {
+    const { query } = this.state
+
     return (
       <div className='app'>
-        <Route path='/search' render={() => (
-          <SearchBooks />
+        <Route path='/search' render={({ history }) => (
+          <DocumentTitle title={query? `${query} - Search - MyReads` : 'Search - MyReads'}>
+            <SearchBooks onUpdateQuery={this.updateQuery} history={history} />
+          </DocumentTitle>
         )} />
 
         <Route exact path='/' render={() => (
-          <ListBooks />
+          <DocumentTitle title='MyReads'>
+            <ListBooks />
+          </DocumentTitle>
         )} />
       </div>
     )
--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -2,35 +2,49 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from '../BooksAPI'
 import Book from './Book'
+import PropTypes from 'prop-types'
 
 const SEARCH_TIMEOUT_IN_MS = 2000
 const MAX_SEARCH_ITEMS = 20
 
 class SearchBooks extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  }
+
   state = {
     results: [],
     typingTimeout: 0,
     isFetching: false
   }
 
-  updateResults = query => {
-    if (query.trim() === '') {
-      this.setState({ results: [] })
-      return
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      const { value } = event.target
+      this.props.onUpdateQuery(value)
+      this.updateResults(value)
     }
+  }
 
+  updateResults = query => {
     if (this.state.typingTimeout) {
       clearTimeout(this.state.typingTimeout);
     }
 
     this.setState({
+      results: [],
+      isFetching: true,
       typingTimeout: setTimeout(() => {
-        BooksAPI.search(query, MAX_SEARCH_ITEMS).then(results => {
-          if (results.hasOwnProperty('error')) results = []
-          this.setState({ results })
-        })
+        this.fetchResults(query)
       }, SEARCH_TIMEOUT_IN_MS)
     });
+  }
+
+  fetchResults = query => {
+    BooksAPI.search(query, MAX_SEARCH_ITEMS).then(results => {
+      if (!results || results.hasOwnProperty('error')) results = []
+      this.setState({ results, isFetching: false })
+    })
   }
 
   render() {
@@ -43,8 +57,8 @@ class SearchBooks extends Component {
           <div className="search-books-input-wrapper">
             <input
               type="text"
-              placeholder="Search by title or author"
-              onChange={event => this.updateResults(event.target.value)}/>
+              placeholder="Search by title or author (and press 'Enter')"
+              onKeyPress={this.handleKeyPress} />
           </div>
         </div>
         <div className="search-books-results">
@@ -53,7 +67,11 @@ class SearchBooks extends Component {
               {results.map(book => <li key={book.id}><Book book={book}/></li>)}
             </ol>
           ) : (
-            <span>Nothing to show here. Maybe you should try another search term? :-)</span>
+            <div className='search-books-message'>
+              {this.state.isFetching?
+                'We\'re fetching your results. Please wait.'
+                : 'Nothing to show here. Maybe you should search for other terms?'}
+            </div>
           )}
         </div>
       </div>
