a4569e2be9aa3b4989a80f7e1d84163d430aca2a
Guilherme Taschetto <gtaschetto@gmail.com>
2017-10-21 18:08:57 -0200

    feat: Add Bookshelf component

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import Book from './Book'
+
+class Bookshelf extends Component {
+  render() {
+    const { books } = this.props
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books && books.length? (
+              <ol className='books-grid'>
+                {books.map(book => <li key={book.id}><Book book={book}/></li>)}
+              </ol>
+            ) : (
+              <div className='search-books-message'>
+                Nothing to show here.
+              </div>
+            )}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Bookshelf
